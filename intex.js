1246032c2cc480146c8718977c60f1ca7c132425
mhdsiyad012-dot <mhdsiyad012@gmail.com>
2026-09-04 11:46:51 +0530

    portfolio

---
+++ intex.js
@@ -0,0 +1,18 @@
+function sendEmail() {
+    const templateParams = {
+        name: document.querySelector("#name").value,
+        email: document.querySelector("#email").value,
+     
+        message: document.querySelector("#message").value,
+    };
+
+    emailjs
+    .send("service_jhrgp0j", "template_mre6h7n", templateParams)
+    .then(() => {
+        alert("Email sent successfully!");
+    })
+    .catch((error) => {
+        console.log("Error sending email:", error);
+        alert("Failed to send email. Please try again.");
+    });
+}
